45804ed76e915989b5c1482f46b4a635ed4ae6d9
andylieou <133063455+andylieou@users.noreply.github.com>
2025-04-27 22:48:45 -0500

    removed bouncing dog

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './dog.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <img src="/milo.png" alt="doggy" className="doggy-sprite" />
+    {/* <img src="/milo.png" alt="doggy" className="doggy-sprite" /> */}
     <App />
   </React.StrictMode>
 )
